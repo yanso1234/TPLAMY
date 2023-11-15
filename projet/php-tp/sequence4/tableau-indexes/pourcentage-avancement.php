7ed360ef565af568704475307db02284d3dd863b
yanis <bouvetyanis@lyceepergaud.fr>
2023-11-15 17:47:37 +0100

    suite tp php2

--- projet/php-tp/sequence4/tableau-indexes/pourcentage-avancement.php
+++ projet/php-tp/sequence4/tableau-indexes/pourcentage-avancement.php
@@ -8,6 +8,6 @@ $texteRouge = "\033[31m";
 foreach ($tableau as $index => $element) {
     $pourcentage_avancement = ($index + 1) / $taille_tableau * 100;
     $normal = "\033[0m";
-    echo $normal . "en cours : " . $texteRouge .  number_format($pourcentage_avancement, 2)  . "%\n" . $normal;
+    echo $normal . "en cours  : " . $texteRouge .  number_format($pourcentage_avancement, 2)  . "%\n" . $normal;
 }
 // echo " élements on éte traités. ";
