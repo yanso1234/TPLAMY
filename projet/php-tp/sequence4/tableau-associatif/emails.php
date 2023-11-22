dbb56a83ca1f1a624a79339fb02b35e9a33c76f6
yanis <bouvetyanis@lyceepergaud.fr>
2023-11-22 16:57:29 +0100

    monno

--- projet/php-tp/sequence4/tableau-associatif/emails.php
+++ projet/php-tp/sequence4/tableau-associatif/emails.php
@@ -4,7 +4,13 @@ $emails = [
     "Monnot" => "monnot@gmail.com",
     "Loberst" => "loberst@laposte.net",
 ];
+$utilisateurs = readline("Saisir un nom de domaine : ");
 foreach ($emails as $étudiant => $email) {
+    $nomded=explode("@",$email,2);
+    if ($nomded[1]== $utilisateurs)
     echo "$étudiant : $email";
-    echo PHP_EOL;
 }
+
+
+
+
