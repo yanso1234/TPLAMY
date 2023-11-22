47af2d4cf989a1239fc1ac806827c1f2ec4b83ff
yanis <bouvetyanis@lyceepergaud.fr>
2023-11-22 17:39:56 +0100

    dm

--- projet/php-tp/sequence4/tableau-associatif/contacts.php
+++ projet/php-tp/sequence4/tableau-associatif/contacts.php
@@ -1,11 +1,16 @@
 <?php
-$noms = [
-    "Grégoire0" => "+0781903243",
-    "Grégoire1" => "+0239665656",
-    "Grégoire2" => "+4526449564",
-    "Grégoire3" => "+9532656564",
-
+$contacts = [
+    "Grégoire0" => "+33 8 65 23 18 15",
+    "Grégoire1" => "+33 9 89 56 42 98",
+    "Grégoire2" => "+33 6 55 99 55 99 ",
 ];
-$utilisateurs = readline("Saisir un numéros : ");
+$nomRecherche = readline("Saisir un nom à rechercher : ");
+if (array_key_exists($nomRecherche, $contacts)) {
+    $numeroTelephone = $contacts[$nomRecherche];
+    echo "Le numéro de téléphone de $nomRecherche est : $numeroTelephone\n";
+} else {
+    echo "Le nom suivant :  $nomRecherche , n'a pas été trouvé.\n";
+}
 
+?>
 
